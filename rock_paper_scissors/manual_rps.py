02df718575131206a96fc2fdafbb899b890016da
chair03 <64752456+chair03@users.noreply.github.com>
2022-09-28 23:23:30 +0100

    Adds playing logic

---
+++ rock_paper_scissors/manual_rps.py
@@ -0,0 +1,26 @@
+import imp
+
+
+import random
+
+def get_computer_choice()->str:
+    return random.choice(['rock','paper','scissors'])
+
+def get_usr_choice()-> str:
+    return input('Enter your choice: ')
+
+def get_winner(computer_choice:str,user_choice:str)->str:
+    winner_dict = {'rock':'paper','scissors':'rock','paper':'scissors'}
+    
+    victor_vs_computer = winner_dict[computer_choice]
+    victor_vs_user = winner_dict[user_choice]
+
+    if victor_vs_computer == user_choice:
+        print('User wins')
+    
+    elif victor_vs_user == computer_choice:
+        print('Computer wins')
+
+    else:
+        print(f'Nobody won, user chose {user_choice} and computer chose {computer_choice}')
+
