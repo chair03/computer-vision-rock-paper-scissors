196d453581696a987eb2c0f8d4f7e8bbcd9ba2d4
chair03 <64752456+chair03@users.noreply.github.com>
2022-09-29 15:08:53 +0100

    Updates changes

--- rock_paper_scissors/manual_rps.py
+++ rock_paper_scissors/manual_rps.py
@@ -1,31 +1,54 @@
-import imp
-
-
 import random
+from camera_rps import rock_paper_scissors_model
 
-def get_computer_choice()->str:
-    return random.choice(['rock','paper','scissors'])
+class Hangman:
+    def __init__(self) -> None:
+        self.computer_wins = 0
+        self.user_wins = 0
+        self.model = rock_paper_scissors_model()
 
-def get_usr_choice()-> str:
-    return input('Enter your choice: ')
+    def get_computer_choice(self)->str:
+        return random.choice(['rock','paper','scissors'])
 
-def get_winner(computer_choice:str,user_choice:str)->str:
-    winner_dict = {'rock':'paper','scissors':'rock','paper':'scissors'}
+    '''
+    Gets user's choice by taking the output of the model which has the largest probability
+    '''
+
+    def get_usr_choice(self)-> str:
+        prediction = self.model.get_user_prediction()
+        return self.model.get_prediction(prediction)
     
-    victor_vs_computer = winner_dict[computer_choice]
-    victor_vs_user = winner_dict[user_choice]
-
-    if victor_vs_computer == user_choice:
-        print('User wins')
+    '''
+    Gets the winner by following the usual definitions of the game 
+    '''
+    def get_winner(self,computer_choice:str,user_choice:str)->str:
+        winner_dict = {'rock':'paper','scissors':'rock','paper':'scissors','None':'None'}
     
-    elif victor_vs_user == computer_choice:
-        print('Computer wins')
+        victor_vs_computer = winner_dict[computer_choice]
+        victor_vs_user = winner_dict[user_choice]
+        if victor_vs_computer == user_choice:
+            print('User wins')
+            return 1,0
+    
+        elif victor_vs_user == computer_choice:
+            print('Computer wins')
+            return 0,1
 
-    else:
-        print(f'Nobody won, user chose {user_choice} and computer chose {computer_choice}')
+        else:
+            print(f'Nobody won, user chose {user_choice} and computer chose {computer_choice}')
+            return 0,0
 
-def play():
-
-    computer_choice = get_computer_choice()
-    user_choice = get_usr_choice()
-    get_winner(computer_choice=computer_choice,user_choice=user_choice)
+    '''
+    Plays the game between the user and the computer
+    '''
+    def play(self)->None:
+        keep_going = True
+        while(keep_going):
+            computer_choice = self.get_computer_choice()
+            print('Hold your move to the camera')
+            user_choice = self.get_usr_choice()
+            user_won,computer_won = self.get_winner(computer_choice=computer_choice,user_choice=user_choice)
+            self.user_wins += user_won
+            self.computer_wins += computer_won
+            if (self.computer_wins == 3) or (self.user_wins == 3):
+                keep_going = False
